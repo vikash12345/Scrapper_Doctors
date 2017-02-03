8c2f4e594be30b054f864c28d0f2ae5365169277
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 14:23:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -25,3 +25,19 @@
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
 ?>
+
+
+<?php
+ require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+$MAX_ID = 3;
+
+for($id = 1; $i <= $MAX_ID; $id++)
+{
+	$MyWebsite = scraperwiki::scrape("http://www.mciindia.org/ViewDetails.aspx?ID=".$id);
+	$html  = file_get_html($MyWebsite);
+	echo $html;
+	
+}
+
+?>
